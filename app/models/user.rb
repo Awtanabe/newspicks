a122b99497588c85da6fba0d965bd7a13f82c370
熊井誠 <sei.kumai@gmail.com>
2018-05-03 14:22:31 +0900

    view作成,haml変換

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :likes
+  has_many :articles
+  has_many :picks
+  has_many :relashonships
 end
